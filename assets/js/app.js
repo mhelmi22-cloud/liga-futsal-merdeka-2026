5abfbbec90b68bb2d351f4e5f02c5b9f7eedc4f3
mhelmi22-cloud <mhelmi22@gmail.com>
2026-07-23 16:54:45 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,35 @@
+window.onload = async ()=>{
 
+    loadSchedule();
+
+}
+
+async function loadSchedule(){
+
+    const matches = await getSchedule();
+
+    let html="";
+
+    matches.forEach(match=>{
+
+        html += `
+        <div style="padding:15px;border-bottom:1px solid #333">
+
+            <b>${match.Home}</b>
+
+            VS
+
+            <b>${match.Away}</b>
+
+            <br>
+
+            ${match.Tarikh} | ${match.Masa}
+
+        </div>
+        `;
+
+    });
+
+    document.getElementById("scheduleList").innerHTML=html;
+
+}
