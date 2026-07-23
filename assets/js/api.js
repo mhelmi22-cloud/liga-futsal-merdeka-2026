d642ab709b906b2c8e6b4e2860943b748616c1a7
mhelmi22-cloud <mhelmi22@gmail.com>
2026-07-23 16:47:51 +0800

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,18 @@
+const API =
+"https://script.google.com/macros/s/AKfycbwmvYbfD0LQaEE1iFJq1R14W6D-qJg58RhPAwxTuOBXQqVdbE_uzAkQC1RlH4zgiHyR/exec";
 
+async function getSchedule(){
+
+    const res = await fetch(API + "?action=schedule");
+
+    return await res.json();
+
+}
+
+async function getStanding(){
+
+    const res = await fetch(API + "?action=standing");
+
+    return await res.json();
+
+}
